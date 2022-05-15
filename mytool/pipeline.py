722debcdece58783eee16083a6f28e9c6cc8b276
huylgia <lghuyvn2000@gmail.com>
2022-05-15 08:58:12 +0000

    simple2 version

--- mytool/pipeline.py
+++ mytool/pipeline.py
@@ -15,7 +15,7 @@ class Predictor():
         self.root_model_dir = root_model_dir
         
         self.image_dir = image_dir
-        self.croped_dir = os.path.dirname(saved_result_dir, "croped")
+        self.croped_dir = os.path.join(saved_result_dir, "croped")
         
         self.writer = FormatWriter()
         
@@ -25,12 +25,12 @@ class Predictor():
         #Detector
         deter = DetPredictor(self.image_dir, self.saved_result_dir, root_model_dir = self.root_model_dir)
         det_file = deter.detect(det_dicts[0])
-        missed_images = deter.process_det(self.cropped_dir, det_file, self.writer)
+        missed_images = deter.process_det(self.croped_dir, det_file, writer = self.writer)
         
         if missed_images:
             missed_det_file, missed_dir = process_missed_det(missed_images, det_dicts[1],
-                               det_file, root_model_dir = self.root_model_dir)
-            deter.process_det(self.cropped_dir, missed_det_file, self.writer)
+                                                             det_file, root_model_dir = self.root_model_dir)
+            deter.process_det(self.cropped_dir, missed_det_file, writer = self.writer)
             shutil.rmtree(missed_dir)
 
         #Recognitor
@@ -48,8 +48,8 @@ class Predictor():
         rec_file = os.path.join(os.path.dirname(rec_file), rec_name_ext)
         esemble_rec(rec_file, rec_dict_list)
         
-        lower_confidence_images = recer.process_rec(rec_file)
-        rename_cropimage(rec_file, self.cropped_dir)
+        lower_confidence_images = recer.process_rec(rec_file, writer = self.writer)
+        rename_cropimage(rec_file, self.croped_dir)
         
         #Save result
         self.writer.write_paddle(self.saved_result_dir + "/Label.txt")
